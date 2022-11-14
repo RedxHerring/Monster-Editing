8b009224a5385024d2ceb81286bb332aa81b6697
RedxHerring <mathwordsmith@gmail.com>
2022-11-13 20:28:16 -0500

    new episodes added and names standardized

--- prep_files.sh
+++ prep_files.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 cd Orig
-# for i in *.mkv; do ffmpeg -i "$i" -map 0:v -c copy "../MP4/${i%.*}.mp4" ;done
-#for i in *.mkv; do mkvextract tracks "$i" 3:"../Subs/${i%.*}1.srt"; done
+# Uncomment the following to extract subtitles from the 1080p source. Change the track selected as needed.
 # for i in *.mkv; do mkvextract tracks "$i" 4:"../Subs/${i%.*}TitlesnSigns.srt"; done
-#for i in *.mkv; do mkvextract tracks "$i" 5:"../Subs/${i%.*}3.srt"; done
-#for i in *.mkv; do mkvextract tracks "$i" 6:"../Subs/${i%.*}4.srt"; done
-# for i in *.mkv; do ffmpeg -i "$i" -vn -an -codec:s:0 srt "../Subs/${i%.mkv}.srt"; done
-# for i in *.mkv; do ffmpeg -i "$i" -vn -an -codec:s:0 ass "../Subs/${i%.mkv}.ass"; done
+
+# Uncomment the following to extract the English audio from the 1080p mkv source. Change the mapping as needed to select different languages
 for i in *.mkv; do ffmpeg -i "$i" -map 0:2 "../WAV/${i%.*}LR.wav" ;done
 cd -
+# Note: Chapter/Episode 15 has an extra audio track of Japanese with a different version of the "Be My Baby" song, so for this case you will have to use the above line as a standard and then extract the desired audio channel
+
+# If using the 720p source from YouTube, the mappings are a bit different, so use this command instead
+# cd Orig720p
+# for i in *.mkv; do ffmpeg -i "$i" -vn "../WAV/${i%.*}LR.wav" ;done
+# cd - 
