1c250b8883523155a9fbaa19615d2fadd9c936a4
RedxHerring <mathwordsmith@gmail.com>
2022-11-21 09:51:53 -0500

    added more episodes

--- prep_files.sh
+++ prep_files.sh
@@ -4,7 +4,15 @@ cd Orig
 # for i in *.mkv; do mkvextract tracks "$i" 4:"../Subs/${i%.*}TitlesnSigns.srt"; done
 
 # Uncomment the following to extract the English audio from the 1080p mkv source. Change the mapping as needed to select different languages
-for i in *.mkv; do ffmpeg -i "$i" -map 0:2 "../WAV/${i%.*}LR.wav" ;done
+file_path="../WAV/"
+suffix="LR.wav"
+for i in *.mkv; do
+    if test -f "$file_path${i%.*}$suffix"; then
+        echo "$file_path${i%.*}$suffix already exists"
+    else
+        ffmpeg -i "$i" -map 0:2 "$file_path${i%.*}$suffix" ;
+    fi
+done
 cd -
 # Note: Chapter/Episode 15 has an extra audio track of Japanese with a different version of the "Be My Baby" song, so for this case you will have to use the above line as a standard and then extract the desired audio channel
 
