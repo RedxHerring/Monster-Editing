1f2165bae28e925efb451f481d3090e485645806
redxherring <mathwordsmith@gmail.com>
2023-03-22 22:52:28 -0400

    storing all times

--- add_titlesnsigns.py
+++ add_titlesnsigns.py
@@ -8,6 +8,8 @@ nlp = spacy.load("en_core_web_lg") # large language model
 # e.g.
 # python add_titlesnsigns.py Subs/NFLX-Subs/eng/Ep01.ass Subs/Full-Subs/eng/Ep01-titlesnsigns.ass
 
+def num_less_than(sequence, value):
+    return len([item for item in sequence if item < value])
 
 main_sub = sys.argv[1]
 titles_sub = sys.argv[2]
@@ -15,6 +17,8 @@ titles_sub = sys.argv[2]
 time_thresh = 5 # seconds within which we'll look for the title line
 added_lines = 0 # iterate when we insert instead of replacing, ie when replacing one line with multiple lines
 popped_lines = []
+line_times = []
+idx0 = -1 # index of first dialogue line
 with open(main_sub,'r') as mainf, open(titles_sub, 'r') as titlesf:
     # Reading the content of the file
     # using the read() function and storing
@@ -29,27 +33,33 @@ with open(main_sub,'r') as mainf, open(titles_sub, 'r') as titlesf:
             line_list = linet.split(',')
             # Find start time of title sub line
             t_startt = datetime.datetime.strptime(line_list[1],'%H:%M:%S.%f') 
+            t_start_numt = t_startt.minute*60 + t_startt.second + t_startt.microsecond/1e6
             line_list = linet.split(',,') # assume nothing in Effect column
-            text_t = line_list[-1].lower() # actual subtitle text, force lowercase
-            doct = nlp(text_t)
+            text_t = line_list[-1] # actual subtitle text
+            doct = nlp(text_t.lower()) # force lowercase
             # Now loop through main file to check if a similaar sub is already there, and either replace or insert
             inserted = False
             for km,linem in enumerate(linesm) :
                 if linem.find("Dialogue:") != -1:
+                    if idx0 == -1:
+                        idx0 = km # set index that we can use later
                     line_list = linem.split(',')
                     # Find start time of title sub line
                     t_startm = datetime.datetime.strptime(line_list[1],'%H:%M:%S.%f') 
+                    line_times.append(t_startm.minute*60 + t_startm.second + t_startm.microsecond/1e6)
                     line_list = linem.split(',,') # assume nothing in Effect column
                     text_m = line_list[-1].lower()
                     docm = nlp(text_m)
                     if abs((t_startm - t_startt).total_seconds()) < time_thresh:
-                        if doct.similarity(docm) > .7 and km not in popped_lines:
-                            lines_out.pop(km+added_lines) # remove this line since we're adding a new version
-                            added_lines -= 1
+                        if doct.similarity(docm) > .5 and km not in popped_lines:
+                            idxi = idx0 + num_less_than(line_times, line_times[-1])
+                            line_times.pop(-1)
+                            lines_out.pop(idxi) # remove this line since we're adding a new version
                             popped_lines.append(km)
                     if t_startm > t_startt and not inserted:
-                        lines_out.insert(km+added_lines+1,text_t)
-                        added_lines += 1
+                        idxi = idx0 + num_less_than(line_times, t_start_numt)
+                        lines_out.insert(idxi,linet)
+                        line_times.append(t_start_numt)
                         inserted = True
                     if (t_startm - t_startt).total_seconds() > time_thresh:
                         break
