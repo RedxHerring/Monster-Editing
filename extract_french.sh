d5930deec923740b0a00438fbaf0cb5052689b2b
redxherring <mathwordsmith@gmail.com>
2023-09-29 09:14:45 -0400

    10-bit encode

--- extract_french.sh
+++ extract_french.sh
@@ -1,50 +1,25 @@
 # We will use this file to get the subs from the 540p source from https://nyaa.si/view/1611098
-
+pwd0=pwd
 cd '[Community] Monster [MULTI DVDRIP 540p x265 AC3]'
 
-file_path="../Subs/French-Subs-unedited"
+subs_path="$pwd0/Subs/French-Subs-unedited"
 mkdir -p $file_path
+mkdir -p "$file_path/fre/"
 suffix=".ass"
 for vid in *.mkv; do
-    for strm in {0..2}; do
-        lang_text=$(ffprobe -v error -hide_banner -of default=noprint_wrappers=0 -print_format flat  -select_streams s:$strm -show_entries stream=index:stream_tags=language $vid | grep language)
-        lang=$(echo $lang_text | cut -d '"' -f 2)
-        echo "Processing video $vid, stream s:$strm, language $lang"
-        mkdir -p "$file_path/$lang/"
-        # For the name we save, we want to match the format used by the Upscale project, which is
-        # EpXXepisodetitle.ass. The title is actually not contained, so we settle for EpXX.ass
-        searchstring="E"
-        rest=${vid#*$searchstring}
-        epnum=$(cut -c 1-2<<< $rest)
-        out_name="Ep$epnum"
-        ffmpeg -y -i "$vid" -map s:$strm "$file_path/$lang/$out_name$suffix"
-    done
-done
-
-cd ../Subs
-
-
-cd ../../NFLX-Subs
-
-# All subtitles need to be formatted correctly for 1080p
-for sub in $(find . -type f -name '*.ass'); do
-    echo "Shifting times for $sub"
-    python ../../shift_sub_times.py $sub -1.5 # move sub times 2 seconds earlier
-done
-
-cd eng
-# Add all available titles and signs lines to each track
-for sub in *.ass; do
-    searchstring="Ep"
-    rest=${sub#*$searchstring}
+    # For the name we save, we want to match the format used by the Upscale project, which is
+    # EpXXepisodetitle.ass. The title is actually not contained, so we settle for EpXX.ass
+    searchstring="Monster "
+    rest=${vid#*$searchstring}
     epnum=$(cut -c 1-2<<< $rest)
-    sub_titles="../../Orig-Subs/eng/Ep$epnum-titlesnsigns.ass"
-    awk '/Title,,/' $sub_titles >> $sub
+    out_name="Ep$epnum"
+    echo "Processing video $vid, $out_name"
+    # Get sub files
+    # for strm in {0..1}; do
+    #     echo "Processing video $vid, stream s:$strm, language fre"
+        
+    #     ffmpeg -y -i "$vid" -map s:$strm "$file_path/$lang/$out_name$suffix"
+    # done
+    # Now extract new fonts
+    ffmpeg -dump_attachment:t "" -i "$file" -n 
 done
-cd ../ # back to NFLX-Subs
-
-sh ../../edit_subs_in_dir.sh .
-
-cd ../
-zip -r NFLX-Subs.zip NFLX-Subs > nul
-rm nul
--- rip_blu-ray.sh
+++ rip_blu-ray.sh
@@ -25,15 +25,11 @@ for vid in $rip_path/*mpg; do
         echo "$fullname_out already exists"
     else
         echo "re-encoding $vid into $fullname_out."
-
         ffmpeg -y -loglevel verbose -init_hw_device qsv=hw -filter_hw_device hw -i $vid -map v:0 -map a:0 -map a:1 -map s:0 -map s:1 -vf "crop=1440:1080" \
-        -c:v av1_qsv -preset veryslow  -global_quality 25 -c:a libvorbis -q:a:0 7 -c:s copy "$fullname_out"
-
-        # ffmpeg -y -loglevel verbose -init_hw_device qsv=hw -filter_hw_device hw -i $vid -map v:0 -map a:0 -map a:1 -map s:0 -map s:1 -vf "crop=1440:1080" \
-        # -c:v av1_qsv -preset veryslow -global_quality:v 10 -look_ahead 1 -extbrc 0 -look_ahead_depth 36 -c:a:0 libvorbis -q:a:0 7 -c:a:1 libopus -b:a:1 108000 -vbr:a on -c:s copy "$fullname_out"
+        -c:v av1_qsv -preset veryslow -global_quality:v 20 -look_ahead 1 -extbrc 0 -look_ahead_depth 36 -pix_fmt yuv420p10le -c:a:0 libvorbis -q:a:0 7 -c:a:1 libopus -b:a:1 108000 -vbr:a on -c:s copy "$fullname_out"
         fullname_out="$out_path/$out_name-h265.mkv"
         echo "re-encoding $vid into $fullname_out."
         ffmpeg -y -loglevel warning -init_hw_device qsv=hw -filter_hw_device hw -i $vid -map v:0 -map a:0 -map a:1 -map s:0 -map s:1 -vf "crop=1440:1080" \
-        -c:v hevc_qsv -preset veryslow -global_quality:v 10 -look_ahead 1 -scenario:v archive -c:a:0 libvorbis -q:a:0 7 -c:a:1 libopus -b:a:1 108000 -vbr:a on -c:s copy "$fullname_out"
+        -c:v hevc_qsv -preset veryslow -global_quality:v 18 -look_ahead 1 -scenario:v archive -pix_fmt p010le -c:a:0 libvorbis -q:a:0 7 -c:a:1 libopus -b:a:1 108000 -vbr:a on -c:s copy "$fullname_out"
     fi
 done
