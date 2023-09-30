72a9765a7cd76213646853f0b7b2353400d0347d
redxherring <mathwordsmith@gmail.com>
2023-09-29 23:57:17 -0400

    including .ass

--- extract_french.sh
+++ extract_french.sh
@@ -1,25 +1,27 @@
 # We will use this file to get the subs from the 540p source from https://nyaa.si/view/1611098
-pwd0=pwd
-cd '[Community] Monster [MULTI DVDRIP 540p x265 AC3]'
+pwd0=$(pwd)
+Fr_dir="$pwd0/[Community] Monster [MULTI DVDRIP 540p x265 AC3]"
 
 subs_path="$pwd0/Subs/French-Subs-unedited"
-mkdir -p $file_path
-mkdir -p "$file_path/fre/"
-suffix=".ass"
-for vid in *.mkv; do
+mkdir -p $subs_path
+mkdir -p "$subs_path/fre/"
+cd $subs_path
+mkdir -p "$pwd0/Audio"
+mkdir -p "$pwd0/Audio/fre"
+for vid in "$Fr_dir"/*.mkv; do
     # For the name we save, we want to match the format used by the Upscale project, which is
     # EpXXepisodetitle.ass. The title is actually not contained, so we settle for EpXX.ass
     searchstring="Monster "
-    rest=${vid#*$searchstring}
+    rest=${vid#*$searchstring} # find first Monster
+    rest=${rest#*$searchstring} # find second Monster
     epnum=$(cut -c 1-2<<< $rest)
     out_name="Ep$epnum"
     echo "Processing video $vid, $out_name"
+    # Japanese first, French second, only want French
+    ffmpeg -n -loglevel warning -i "$vid" -map a:1 -c:a flac "$pwd0/Audio/fre/$out_name.flac"
     # Get sub files
-    # for strm in {0..1}; do
-    #     echo "Processing video $vid, stream s:$strm, language fre"
-        
-    #     ffmpeg -y -i "$vid" -map s:$strm "$file_path/$lang/$out_name$suffix"
-    # done
+    ffmpeg -n -loglevel warning -i "$vid" -map s:0 "$subs_path/fre/$out_name.ass"
+    ffmpeg -n -loglevel warning -i "$vid" -map s:1 "$subs_path/fre/$out_name-titlensigns.ass"
     # Now extract new fonts
-    ffmpeg -dump_attachment:t "" -i "$file" -n 
+    ffmpeg -n -loglevel warning -dump_attachment:t "" -i "$vid" -n 
 done
