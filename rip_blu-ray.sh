1a0b613945c28093b215ef5851176d8a40261aa4
redxherring <mathwordsmith@gmail.com>
2023-11-11 23:38:20 -0500

    large_v3 implemented

--- rip_blu-ray.sh
+++ rip_blu-ray.sh
@@ -9,7 +9,7 @@ rip_path="Blu-Ray/Ripped-Titles"
 mkdir -p $rip_path
 
 for title in {0..15}; do
-    mplayer -dumpstream br://$title -nocache -bluray-device $1 -dumpfile "$rip_path/title$title.mpg"
+    mplayer -dumpstream br://$title -nocache -bluray-device "$1" -dumpfile "$rip_path/title$title.mpg"
 done
 echo "Titles from $1 saved in $rip_path. View and rename them, deleting any repeats or undesired videos."
 read -p "When finished renaming, press Enter to start encoding process." </dev/tty
--- transcribe_audio.sh
+++ transcribe_audio.sh
@@ -9,6 +9,7 @@ lang=$(echo $(basename $1))
 mkdir -p Subs/Whisper-Transcribed/$lang
 lan=$(cut -c 1-2<<< $lang)
 for afile in "$1"*.flac; do
-    whisper "$afile" --model large-v2 --task transcribe --language $lan --beam_size 10 --patience 2   --suppress_tokens "" --condition_on_previous_text False --output_format srt --output_dir Subs/Whisper-Transcribed/$lang/
+    echo "Transcribing $afile into  Subs/Whisper-Transcribed/$lang/"
+    whisper "$afile" --model large-v3 --task transcribe --language $lan --beam_size 10 --patience 2   --suppress_tokens "" --condition_on_previous_text False --output_format srt --output_dir Subs/Whisper-Transcribed/$lang/
 done
 # sudo intel_gpu_top
