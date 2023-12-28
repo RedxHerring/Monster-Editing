00404da73c2bfe48410ed7021870ca86092d1e19
redxherring <mathwordsmith@gmail.com>
2023-12-28 12:08:55 -0500

    single av1 encode

--- av1_encode.sh
+++ av1_encode.sh
@@ -1,10 +1,11 @@
 # This bash script will take files output from kdenlive, ideally lossless ones, and re-encode as av1 to beocme much smaller
 
-cd Output/
+cd $1
 
-for i in *.mp4; do
-    ffmpeg -init_hw_device qsv=hw -filter_hw_device hw -i "$i" -c:v av1_qsv "${i%.*}GPU.mkv"
+for vid in *.mp4; do
+    ffmpeg -n -loglevel warning -init_hw_device qsv=hw -filter_hw_device hw -i $vid -map 0 -c:v av1_qsv -preset veryslow \
+        -global_quality:v 20 -extbrc 0 -look_ahead_depth 36 -pix_fmt p010le -c:a:0  libopus -b:a 108000 -vbr:a on "${vid%.*}.mkv"
 done
 # ffmpeg -init_hw_device qsv=hw -filter_hw_device hw -f rawvideo -pix_fmt yuv420p -s:v 1920x1080 -i Monster_1-4_Murder_and_Execution_1080p_24fps.mp4 -vf hwupload=extra_hw_frames=64,format=qsv -c:v av1_qsv -b:v 5M Monster_1-4_Murder_and_Execution_1080p_24fps_GPU.mkv
-
+# burn subs using https://trac.ffmpeg.org/wiki/HowToBurnSubtitlesIntoVideo
 # https://trac.ffmpeg.org/wiki/Hardware/QuickSync
