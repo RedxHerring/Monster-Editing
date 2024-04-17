258cccd2e27c70e8f833666cfe352a45724f1e7e
redxherring <mathwordsmith@gmail.com>
2024-04-16 23:38:15 -0400

    building single-file workflow first

--- translate.py
+++ translate.py
@@ -1,85 +1,87 @@
-## Transcribe and translate .wav or .srt files into a desired language.
+# Transcribe and translate .wav or .srt files into a desired language.
 # The idea is to use whisper for an initial guess and to get timestamps,
 # and then to use those timestamps to feed wav segments into seamlessm4t
 import sys
-from whisper.tokenizer import LANGUAGES, TO_LANGUAGE_CODE, get_tokenizer
+import os
+from pathlib import Path
+from whisper.utils import get_writer
 from whisper.transcribe import cli
+from seamless_communication.models.inference import Translator
+
+# This module should act as a drop-in placement for whisper, and therefore should be able to be run as below
 # whisper japanese.wav --language Japanese --task translate
+# It should also be possible to input a directory, 
+# and it will recursively pass the additional arguments for each option
 
-def cli1():
-
+def clim4t():
     # fmt: off
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("audio", nargs="+", type=str, help="audio file(s) to transcribe")
-    parser.add_argument("--model", default="large", help="name of the Whisper model to use for transcribing")
-    parser.add_argument("--model_dir", type=str, default=None, help="the path to save model files; uses ~/.cache/whisper by default")
-    parser.add_argument("--device", default="cuda" if torch.cuda.is_available() else "cpu", help="device to use for PyTorch inference")
     parser.add_argument("--output_dir", "-o", type=str, default=".", help="directory to save the outputs")
-    parser.add_argument("--output_format", "-f", type=str, default="srt", choices=["txt", "vtt", "srt", "tsv", "json", "all"], help="format of the output file; if not specified, srt will be produced")
-    parser.add_argument("--verbose", type=str2bool, default=True, help="whether to print out the progress and debug messages")
-
-    parser.add_argument("--task", type=str, default="transcribe", choices=["transcribe", "translate"], help="whether to perform X->X speech recognition ('transcribe') or X->English translation ('translate')")
-    parser.add_argument("--language", type=str, default=None, choices=sorted(LANGUAGES.keys()) + sorted([k.title() for k in TO_LANGUAGE_CODE.keys()]), help="language spoken in the audio, specify None to perform language detection")
-
-    parser.add_argument("--temperature", type=float, default=0, help="temperature to use for sampling")
-    parser.add_argument("--best_of", type=optional_int, default=5, help="number of candidates when sampling with non-zero temperature")
-    parser.add_argument("--beam_size", type=optional_int, default=5, help="number of beams in beam search, only applicable when temperature is zero")
-    parser.add_argument("--patience", type=float, default=None, help="optional patience value to use in beam decoding, as in https://arxiv.org/abs/2204.05424, the default (1.0) is equivalent to conventional beam search")
-    parser.add_argument("--length_penalty", type=float, default=None, help="optional token length penalty coefficient (alpha) as in https://arxiv.org/abs/1609.08144, uses simple length normalization by default")
-
-    parser.add_argument("--suppress_tokens", type=str, default="-1", help="comma-separated list of token ids to suppress during sampling; '-1' will suppress most special characters except common punctuations")
-    parser.add_argument("--initial_prompt", type=str, default=None, help="optional text to provide as a prompt for the first window.")
-    parser.add_argument("--condition_on_previous_text", type=str2bool, default=True, help="if True, provide the previous output of the model as a prompt for the next window; disabling may make the text inconsistent across windows, but the model becomes less prone to getting stuck in a failure loop")
-    parser.add_argument("--fp16", type=str2bool, default=True, help="whether to perform inference in fp16; True by default")
-
-    parser.add_argument("--temperature_increment_on_fallback", type=optional_float, default=0.2, help="temperature to increase when falling back when the decoding fails to meet either of the thresholds below")
-    parser.add_argument("--compression_ratio_threshold", type=optional_float, default=2.4, help="if the gzip compression ratio is higher than this value, treat the decoding as failed")
-    parser.add_argument("--logprob_threshold", type=optional_float, default=-1.0, help="if the average log probability is lower than this value, treat the decoding as failed")
-    parser.add_argument("--no_speech_threshold", type=optional_float, default=0.6, help="if the probability of the <|nospeech|> token is higher than this value AND the decoding has failed due to `logprob_threshold`, consider the segment as silence")
-    parser.add_argument("--word_timestamps", type=str2bool, default=False, help="(experimental) extract word-level timestamps and refine the results based on them")
-    parser.add_argument("--prepend_punctuations", type=str, default="\"\'“¿([{-", help="if word_timestamps is True, merge these punctuation symbols with the next word")
-    parser.add_argument("--append_punctuations", type=str, default="\"\'.。,，!！?？:：”)]}、", help="if word_timestamps is True, merge these punctuation symbols with the previous word")
-    parser.add_argument("--highlight_words", type=str2bool, default=False, help="(requires --word_timestamps True) underline each word as it is spoken in srt and vtt")
-    parser.add_argument("--max_line_width", type=optional_int, default=None, help="(requires --word_timestamps True) the maximum number of characters in a line before breaking the line")
-    parser.add_argument("--max_line_count", type=optional_int, default=None, help="(requires --word_timestamps True) the maximum number of lines in a segment")
-    parser.add_argument("--max_words_per_line", type=optional_int, default=None, help="(requires --word_timestamps True, no effect with --max_line_width) the maximum number of words in a segment")
-    parser.add_argument("--threads", type=optional_int, default=0, help="number of threads used by torch for CPU inference; supercedes MKL_NUM_THREADS/OMP_NUM_THREADS")
-    parser.add_argument("--clip_timestamps", type=str, default="0", help="comma-separated list start,end,start,end,... timestamps (in seconds) of clips to process, where the last end timestamp defaults to the end of the file")
-    parser.add_argument("--hallucination_silence_threshold", type=optional_float, help="(requires --word_timestamps True) skip silent periods longer than this threshold (in seconds) when a possible hallucination is detected")
-    # fmt: on
-
     args = parser.parse_args().__dict__
-    model_name: str = args.pop("model")
-    model_dir: str = args.pop("model_dir")
     output_dir: str = args.pop("output_dir")
-    output_format: str = args.pop("output_format")
     device: str = args.pop("device")
     os.makedirs(output_dir, exist_ok=True)
 
 
-def create_parser():
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("audio", nargs="+", type=str, help="audio file(s) to transcribe")
-    parser.add_argument("--model", default="large", help="name of the Whisper model to use for transcribing")
+def translate_srt(srt_file, out_file, src_lang, tgt_lang):
+    # Initialize a Translator object with a multitask model, vocoder on the GPU.
+    translator = Translator("seamlessM4T_large", vocoder_name_or_card="vocoder_36langs", device=torch.device("cuda"))
+    with open(srt_file,'r') as file:
+        lines = file.readlines()
+        idx = 0
+        # line_types = ['index','timestamp','dialogue']
+        idxt = 0 #  0, 1 or 2
+        while idx < len(lines):
+            if not len(lines[idx]): # empty line, skip
+                idx += 1
+                continue
+            if lines[idx][0].isdigit() and not idxt: # looking for a digit and we got it
+                idx += 1
+                idxt = 1 # Now we want to look for timestamp
+                continue
+            if " --> " in lines[idx][0] and idxt == 1: # timestamp, next is dialogue
+                idx += 1
+                idxt = 2 # Now we want to look for dialogue
+                continue
+            if len(lines[idx]) and idxt != 1: # allow either 0 or 2
+                # Need to get this segment of dialogue  in audio form.
+                # S2TT
+                text_output, _ = translator.predict(
+                    input=path_to_input_audio,
+                    task_str="S2TT",
+                    tgt_lang=tgt_lang,
+                    text_generation_opts=text_generation_opts,
+                    unit_generation_opts=None
+                )
+    with open(out_file,'w') as file:
+        file.writelines(lines)
 
-
-    input_file = sys.argv[1]
-    input_lang = sys.argv[2]
-
-    parser = ArgumentParser()
-    parser.add_argument("model")
-    parser.add_argument("dset")
-    parser.add_argument("--l2_coeff", type=float, default=1e-6)
-
-    if len(sys.argv) > 3 and sys.argv[3] == "-h":
-        parser.print_help()
-        sys.exit(0)
-
-    return parser
+# whisper "$afile" --model large-v2 --task transcribe --language $language --beam_size 10 --patience 2  --suppress_tokens "" --condition_on_previous_text False \
+#         --word_timestamps True  --hallucination_silence_threshold 2 --output_format srt --output_dir Subs/Whisper-Transcribed/$lang/
 
 if __name__ == "__main__":
     # First we run whisper to generate
     if len(sys.argv) == 1: # no cli inputs, run with following parameters
-        model = "large"
+        model_name = "large"
+        audio_path = "audio/spa/Ep01.wav" # input a .wav to use whisper
+        langin = "spa"
+        langout = "eng"
+        output_dir = os.path.join("Subs","Whisper-Transcribed",langout)
+        temperature = 2
+        if (increment := args.pop("temperature_increment_on_fallback")) is not None:
+            temperature = tuple(np.arange(temperature, 1.0 + 1e-6, increment))
+        else:
+            temperature = [temperature]
+        # setup srt location
+        basename = Path(audio_path).stem
+        srtfile = os.path.join(output_dir,audio_path+".srt")
+        from whisper import load_model
+        model = load_model(model_name)
+        result = transcribe(model, audio_path, temperature=temperature, word_timestamps=True, hallucination_silence_threshold=2)
+        writer(result, audio_path)
     else:
+        if not any('--output_format' == x for x in sys.argv):
+            sys.argv.append(" --output_format srt") # make sure we pass the right things to whisper
         cli() # run whisper 
+    translate_srt(srt_file=srtfile, out_file="Subs/out.srt", src_lang="deu", tgt_lang="eng")
